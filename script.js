cdeb4bf1366e95a9a518a4d1cfe20e6cb8be6ee3
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-12-19 20:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.getElementById('employeeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData);
+    
+    let ageVerificationPromise = new Promise((resolved, rejected)=>{
+      if(data.age>18){
+        resolved(data.name);
+      } else {
+        rejected(data.name);
+      }
+    })
+    
+    ageVerificationPromise.then((name)=>{
+      alert("Welcome, "+name+". You can vote.");
+    }).catch((name)=>{
+      alert("Oh sorry "+name+". You aren't old enough.");
+    })
+  });
